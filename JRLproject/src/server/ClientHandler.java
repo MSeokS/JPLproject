88d1b5739eca01742ca302a8c640779392175a9f
BIGDannyK <34018997+BIGDannyK@users.noreply.github.com>
2024-06-09 12:34:08 +0900

    Update ClientHandler.java

--- JRLproject/src/server/ClientHandler.java
+++ JRLproject/src/server/ClientHandler.java
@@ -5,27 +5,27 @@ class ClientHandler implements Runnable {
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
-    private String chatRoom;
     private String clientName;
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
     }
 
+    @Override
     public void run() {
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
 
+            
             clientName = in.readLine();
-            chatRoom = in.readLine();
-            System.out.println(clientName + " joined " + chatRoom);
-            ServerThread.addClientToRoom(chatRoom, this);
+            System.out.println(clientName + " connected.");
+            ServerThread.broadcast(clientName + " has joined the chat", this);
 
             String message;
             while ((message = in.readLine()) != null) {
                 System.out.println("Received message from " + clientName + ": " + message);
-                ServerThread.broadcast(chatRoom, clientName + ": " + message, this);
+                ServerThread.broadcast(clientName + ": " + message, this);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,8 +35,9 @@ class ClientHandler implements Runnable {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            ServerThread.removeClientFromRoom(chatRoom, this);
-            System.out.println(clientName + " disconnected from " + chatRoom);
+            ServerThread.removeClient(this);
+            System.out.println(clientName + " disconnected.");
+            ServerThread.broadcast(clientName + " has left the chat", this);
         }
     }
 
